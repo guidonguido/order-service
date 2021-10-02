9f7c0be54b1e49a6ca64c0be2a972d4c35e711cf
Guido R <guidonguido@gmail.com>
2021-10-02 18:07:43 +0200

    Add validation dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,9 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("javax.validation:validation-api")
+
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
